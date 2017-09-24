8c21cbe3a53ba67ac9d8b0d4ee226af5ac857012
Deepak Kumar <deepak.kumar1@one.verizon.com>
2017-09-24 08:28:31 -0700

    adding structure

--- src/main/java/com/learning/roboticmaneuvers/application/RoboticManeuversApplication.java
+++ src/main/java/com/learning/roboticmaneuvers/application/RoboticManeuversApplication.java
@@ -1,8 +1,20 @@
 package com.learning.roboticmaneuvers.application;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by kumade1 on 9/24/17.
  */
 @SpringBootApplication
 public class RoboticManeuversApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoboticManeuversApplication.class, args);
+
+    }
+
+    public void run(String... args) throws Exception {
+    }
+
 }
